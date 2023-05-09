b9ca47df0e187333235f4fdfeca45f216d88b799
sedatbilece <seda1tbilece@gmail.com>
2023-05-09 16:07:02 +0300

    add(Catalog.API) : DTOs

---
+++ services/catalog/FreeCourse.Services.Catalog/Dtos/CourseUpdateDto.cs
@@ -0,0 +1,30 @@
+﻿namespace FreeCourse.Services.Catalog.Dtos
+{
+    public class CourseUpdateDto
+    {
+
+        public string Id { get; set; }
+        public string Name { get; set; }
+
+
+        public decimal Price { get; set; }
+
+        public string Description { get; set; }
+
+        public string UserId { get; set; }
+
+        public string Picture { get; set; }
+
+
+        public DateTime CreatedDate { get; set; }
+
+
+        // Assosiation
+        public FeatureDto Feature { get; set; }
+
+
+        public string CategoryId { get; set; }
+
+        public CategoryDto Category { get; set; }
+    }
+}
