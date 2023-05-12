06cf9eb30ac178b334c755e98f9801fb7a465919
sedatbilece <seda1tbilece@gmail.com>
2023-05-12 15:01:20 +0300

    refactor(catalog) : appsettings

--- services/catalog/FreeCourse.Services.Catalog/Program.cs
+++ services/catalog/FreeCourse.Services.Catalog/Program.cs
@@ -1,12 +1,19 @@
+using FreeCourse.Services.Catalog.Settings;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddAutoMapper(typeof(Program));
+
+builder.Services.Configure<DatabaseSettings>(builder.Configuration.GetSection("DatabaseSettings"));
+builder.Services.AddSingleton<IDatabaseSettings>(sp =>
+{
+    return sp.GetRequiredService<IOptions<DatabaseSettings>>().Value;
+})
 
 var app = builder.Build();
 
