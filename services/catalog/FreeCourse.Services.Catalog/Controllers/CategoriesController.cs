9cb71d8f2f3344d5d1c97d55ad2b3cbf34cf799d
sedatbilece <seda1tbilece@gmail.com>
2023-05-13 11:39:35 +0300

    add ( Catalog ) : CategoriesController

---
+++ services/catalog/FreeCourse.Services.Catalog/Controllers/CategoriesController.cs
@@ -0,0 +1,47 @@
+﻿using FreeCourse.Services.Catalog.Dtos;
+using FreeCourse.Services.Catalog.Services;
+using FreeCourse.Shared.ControllerBases;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FreeCourse.Services.Catalog.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : CustomBaseController
+    {
+
+        private readonly ICategoryService _categoryService;
+
+        public CategoriesController(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+
+
+        public async Task<IActionResult> GetAll()
+        {
+            var categories = await _categoryService.GetAllAsync();
+
+            return CreateActionResultInstance(categories);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(string id)
+        {
+            var categories = await _categoryService.GetByIdAsync(id);
+
+            return CreateActionResultInstance(categories);
+        }
+
+
+        public async Task<IActionResult> Create(CategoryDto categoryDto)
+        {
+            var categories = await _categoryService.CreateAsync(categoryDto);
+
+            return CreateActionResultInstance(categories);
+        }
+
+
+    }
+}
