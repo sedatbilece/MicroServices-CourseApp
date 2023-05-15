9855bebbc61dabae8a4a2cfcf0d649dfc6a2fc30
sedatbilece <seda1tbilece@gmail.com>
2023-05-15 15:36:59 +0300

    fail in identityserver

--- IdentityServer/FreeCourse.IdentityServer/Program.cs
+++ IdentityServer/FreeCourse.IdentityServer/Program.cs
@@ -2,7 +2,11 @@
 // Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
 
 
+using FreeCourse.IdentityServer.Models;
+using IdentityServer4.EntityFramework.DbContexts;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -37,25 +41,35 @@ namespace FreeCourse.IdentityServer
 
             try
             {
-                var seed = args.Contains("/seed");
-                if (seed)
-                {
-                    args = args.Except(new[] { "/seed" }).ToArray();
-                }
+               
 
                 var host = CreateHostBuilder(args).Build();
 
-                if (seed)
+
+                using (var scope = host.Services.CreateScope())
                 {
-                    Log.Information("Seeding database...");
-                    var config = host.Services.GetRequiredService<IConfiguration>();
-                    var connectionString = config.GetConnectionString("DefaultConnection");
-                    SeedData.EnsureSeedData(connectionString);
-                    Log.Information("Done seeding database.");
-                    return 0;
+                    var services = scope.ServiceProvider;
+
+                var  dbContext  = services.GetRequiredService<ConfigurationDbContext>();
+
+                    dbContext.Database.Migrate();
+
+                    var userManager = services.GetRequiredService<UserManager<ApplicationUser>>();
+
+
+                    if(!userManager.Users.Any()) {
+
+                        userManager.CreateAsync(new ApplicationUser
+                        {
+                            UserName = "sedatblc",
+                            Email = "deneme@gmail.com",
+                        }, "Password123*").Wait();
+                    }
                 }
 
-                Log.Information("Starting host...");
+
+
+                    Log.Information("Starting host...");
                 host.Run();
                 return 0;
             }
